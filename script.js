3ded532ec4687c32a43d62003ad854500b8e239d
Zaid <142177842+zaidbinshams@users.noreply.github.com>
2025-04-27 11:10:07 +0530

    added basic functions

---
+++ script.js
@@ -0,0 +1,7 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => {
+    if (b === 0) return "Err: Division by Zero";
+    else return a / b;
+}
