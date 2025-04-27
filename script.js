713292bd2bdeb99453270d50c402266dcf9a350c
Zaid <142177842+zaidbinshams@users.noreply.github.com>
2025-04-27 15:25:17 +0530

    digit and operation buttons now work

--- script.js
+++ script.js
@@ -1,30 +1,95 @@
 // MATH FUNCTIONS
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => {
+const add = (a = 0, b = 0) => a + b;
+const subtract = (a = 0, b = 0) => a - b;
+const multiply = (a = 1, b = 1) => a * b;
+const divide = (a = 1, b = 1) => {
     if (b === 0) return "Err: Division by Zero";
     else return a / b;
 }
 
-// USER INPUT
-let numOne;
-let numTwo;
-let operator;
+// OPERATOR HANDLING
+let numOne = "";
+let numTwo = "";
+let operator = "";
 
-switch (operator) {
-    case "+":
-        add(numOne, numTwo);
-        break;
-    case "-":
-        subtract(numOne, numTwo);
-        break;
-    case "*":
-        multiply(numOne, numTwo);
-        break;
-    case "/":
-        divide(numOne, numTwo);
-        break;
-    default:
-        return "you tweakin twin";
+const allowedNumbers = "1234567890";
+const numArray = allowedNumbers.split("");
+const allowedOperators = "+-*/";
+const opArray = allowedOperators.split("");
+
+function operate(n1, n2, op) {
+    let answer;
+    switch (op) {
+        case "+":
+            answer = add(n1, n2);
+            break;
+        case "-":
+            answer = subtract(n1, n2);
+            break;
+        case "*":
+            answer = multiply(n1, n2);
+            break;
+        case "/":
+            answer = divide(n1, n2);
+            break;
+        default:
+            answer = "you tweakin twin";
+    }
+    return answer;
 }
+
+// HTML INTERACTION
+const display = document.querySelector("div.display");
+const digits = document.querySelectorAll("button.num");
+const operators = document.querySelectorAll("button.operator");
+const calc = document.querySelector("button.equal");
+
+function enableCalc() {
+
+    function updateDisplayNum(digit) {
+        let opCheck = display.textContent.split("");
+        if (!opCheck.some((char) => opArray.includes(char))) {
+            numOne += digit.textContent;
+            display.textContent = numOne;
+        } else {
+            numTwo += digit.textContent;
+            display.textContent += digit.textContent;
+        }
+        numOne = Number(numOne);
+        numTwo = Number(numTwo);
+    }
+
+    digits.forEach((digit) => {
+        digit.addEventListener("click", (e) => updateDisplayNum(e.target));
+    });
+
+    function updateDisplayOp(op) {
+        let opCheck = display.textContent.split("");
+        if (!opCheck.some((char) => opArray.includes(char))) {
+            if (!opArray.includes(opCheck[opCheck.length-1])) {
+                display.textContent += op.textContent;
+            } else {
+                opCheck.pop();
+                display.textContent = opCheck.join("") + op.textContent;
+            }
+            operator = op.textContent;
+        } else {
+            calc.click();
+            updateDisplayOp(op);
+        }
+    }
+
+    operators.forEach((op) => op.addEventListener("click", (e) => updateDisplayOp(e.target)));
+
+    calc.addEventListener("click", () => {
+        let result = operate(numOne, numTwo, operator);
+        display.textContent = result;
+        numOne = result;
+        numTwo = "";
+    });
+}
+
+enableCalc();
+
+// after the result is diplayed,
+// pressing a new digit doesn't refresh the screen
