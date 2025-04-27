7b88162a49a8145a8f89554fd208b23af87cce1d
Zaid <142177842+zaidbinshams@users.noreply.github.com>
2025-04-27 11:16:40 +0530

    added switch-case for operator handling

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+// MATH FUNCTIONS
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => {
     if (b === 0) return "Err: Division by Zero";
     else return a / b;
+}
+
+// USER INPUT
+let numOne;
+let numTwo;
+let operator;
+
+switch (operator) {
+    case "+":
+        add(numOne, numTwo);
+        break;
+    case "-":
+        subtract(numOne, numTwo);
+        break;
+    case "*":
+        multiply(numOne, numTwo);
+        break;
+    case "/":
+        divide(numOne, numTwo);
+        break;
+    default:
+        return "you tweakin twin";
 }
